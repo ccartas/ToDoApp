342ee983106fd097e7d13c7a092b589887249b5a
Bogdan Iancu <bogdan.iancu@ymail.com>
2018-05-24 12:33:56 +0300

    Code at the halfway of the 9th meeting

--- app/src/main/java/cegeka/scoaladevalori/ro/todoapp/AddToDoActivity.java
+++ app/src/main/java/cegeka/scoaladevalori/ro/todoapp/AddToDoActivity.java
@@ -1,12 +1,20 @@
 package cegeka.scoaladevalori.ro.todoapp;
 
+import android.content.Intent;
+import android.provider.CalendarContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.Toast;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class AddToDoActivity extends AppCompatActivity {
     EditText editTextTitle = null;
@@ -19,23 +27,68 @@ public class AddToDoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_to_do);
 
-        Button buttonAdd = findViewById(R.id.button_add);
-        buttonAdd.setOnClickListener(new View.OnClickListener() {
+        editTextTitle = findViewById(R.id.editTextTitle);
+        editTextDescription = findViewById(R.id.editTextDescription);
+        seekBar = findViewById(R.id.seekBar);
+        editTextDueDate = findViewById(R.id.editTextDueDate);
+
+        Button buttonAddToCalendar = findViewById(R.id.button_calendar);
+        buttonAddToCalendar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ToDoItem item = new ToDoItem();
-                EditText editTextTitle = findViewById(R.id.editTextTitle);
-                item.title = editTextTitle.getText().toString();
-                SeekBar seekBar = findViewById(R.id.seekBar);
-                item.priority = seekBar.getProgress();
+                if(Validate()) {
+                    ToDoItem item = new ToDoItem();
+                    item.title = editTextTitle.getText().toString();
+                    item.priority = seekBar.getProgress();
+                    item.description = editTextDescription.getText().toString();
+                    SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+                    try {
+                        item.dueDate = format.parse(editTextDueDate.getText().toString());
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
 
-                Toast.makeText(AddToDoActivity.this, item.title,
-                        Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(Intent.ACTION_INSERT, CalendarContract.Events.CONTENT_URI);
+                    Calendar beginTime = Calendar.getInstance();
+                    beginTime.setTime(item.dueDate);
+                    intent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, beginTime.getTimeInMillis());
+                    intent.putExtra(CalendarContract.Events.TITLE, item.title);
+                    startActivity(intent);
+                }
             }
         });
     }
 
     private boolean Validate() {
-        return false;
+        if(editTextTitle.getText().toString().trim().equals("") ||
+            editTextDescription.getText().toString().trim().equals("") ||
+            editTextDueDate.getText().toString().trim().equals("")) {
+            Toast.makeText(AddToDoActivity.this,
+                    "All the fields are mandatory",
+                    Toast.LENGTH_LONG).show();
+            return false;
+        }
+        SimpleDateFormat format =
+                new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        Date date = null;
+        try {
+            date =
+              format.parse(editTextDueDate.getText().toString() + " 23:59");
+        } catch (ParseException e) {
+            e.printStackTrace();
+            Toast.makeText(AddToDoActivity.this,
+                    "Invalid date format",
+                    Toast.LENGTH_LONG).show();
+            return false;
+        }
+        Date currentDate = new Date();
+        if(date != null && date.compareTo(currentDate) <= 0) {
+            Toast.makeText(AddToDoActivity.this,
+                    "Date should be at least today",
+                    Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+        return true;
     }
 }
